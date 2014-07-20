74406e28f33414f829a9f495e024643d1835dd7e
James Johnson <johnsojr@gmail.com>
2014-07-20 16:09:26 -0500

    Beginning settings controller.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,7 +1,24 @@
 'use strict';
 
-angular.module('simpleMail', [])
+angular.module('simpleMail', ['ngRoute'])
   .config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+      templateUrl: 'templates/home.html',
+      controller: 'HomeController'
+    })
+    .when('/settings', {
+      templateUrl: 'templates/settings.html',
+      controller: 'SettingsController'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+  })
+  .controller('HomeController', function(){
+
+  })
+  .controller('SettingsController', function(){
 
   });
 
